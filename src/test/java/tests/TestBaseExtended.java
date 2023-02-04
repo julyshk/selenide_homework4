1c3444f95d5bfe21cee1ac65531df42b888c6533
Шкреба Юлия Владимировна <yvshkreba@sberbank.ru>
2023-02-04 23:25:31 +0700

    change TestBaseExtended

--- src/test/java/tests/TestBaseExtended.java
+++ src/test/java/tests/TestBaseExtended.java
@@ -7,7 +7,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
+
+import java.util.Map;
 
 public class TestBaseExtended {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -16,7 +19,15 @@ public class TestBaseExtended {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotest.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
